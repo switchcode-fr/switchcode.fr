8c44819721a36e198461d2fc1b84156d061c3a11
Nassif Bourguig <nbourguig@gmail.com>
2023-09-15 18:45:15 +0200

    Update logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,19 @@ import Head from "next/head";
 import react from "react";
 
 export default function Home() {
-    return (
-        <div>
-            <Head>
-                <title>switchcode.fr</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <div className="flex flex-col items-center justify-around p-20 space-y-3">
-                <img src="/switchcode-logo-100.svg" alt="switchcode logo"/>
-                <h1 className="inline-block italic font-light text-white">
-                    switchcode.fr
-                </h1>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <Head>
+        <title>switchcode.fr</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex flex-col items-center justify-around p-20 space-y-3">
+        <img src="/logo-1600x400.svg" alt="switchcode logo" />
+        <h1 className="inline-block italic font-light text-white">
+          switchcode.fr
+        </h1>
+      </div>
+    </div>
+  );
 }
